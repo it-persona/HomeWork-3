f2f978a94534c9f3fe75606ba92390cf80c53330
Vadym Panchenko <panchenko.vadym@gmail.com>
2014-11-01 04:10:49 +0200

    Added exception handling error 404

--- index.php
+++ index.php
@@ -19,6 +19,14 @@ $router->get('/animals', [$animalController, 'getAnimalsAction']);
 $router->get('/animals/{animalId}', [$animalController, 'getAnimalAction']);
 
 $dispatcher = new Phroute\Dispatcher($router);
-$response = $dispatcher->dispatch($request->getMethod(), parse_url($request->getPathInfo(), PHP_URL_PATH));
 
+try {
+    $response = $dispatcher->dispatch($request->getMethod(), parse_url($request->getPathInfo(), PHP_URL_PATH));
+} catch (Phroute\Exception\HttpRouteNotFoundException $e) {
+    print('<b style="color: red">Error 404: Page not found</b>');
+    die();
+} catch (Phroute\Exception\HttpMethodNotAllowedException $e) {
+    var_dump($e);
+    die();
+}
 $response->send();
