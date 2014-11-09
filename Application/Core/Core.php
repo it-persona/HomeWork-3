409994823843dc9c6e7d20cb5f90a42287ba0e5c
Vadym Panchenko <panchenko.vadym@gmail.com>
2014-11-10 00:58:59 +0200

    MVC application (old version)

--- Application/Core/Core.php
+++ Application/Core/Core.php
@@ -5,7 +5,7 @@
  * @author      Vadym Panchenko <panchenko.vadym@gmail.com>
  * @link        http://www.it-persona.com.ua/
  * @copyright   2010-2014 IT-PERSONA
- * @license     http://www.it-persona.com.ua/license/
+ *
  */
 
 abstract class Core
