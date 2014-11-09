10405c51328548bb7fbbbf36ffde5a59c2217811
Vadym Panchenko <panchenko.vadym@gmail.com>
2014-11-10 00:38:56 +0200

    Old version MVC application on PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * index.php
+ *
+ * @author      Vadym Panchenko <panchenko.vadym@gmail.com>
+ * @link        http://www.it-persona.com.ua/
+ * @copyright   2010-2014 IT-PERSONA
+ * @license     http://www.it-persona.com.ua/license/
+ */
+
+ini_set('display_errors', 1);
+require_once 'Application/bootstrap.php';
